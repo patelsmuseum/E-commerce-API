bc06e4526e878c394bc27cbc8496f5862af70939
Purshotam <pankajpurshotam@gmail.com>
2023-09-06 00:44:30 +0530

    functionality completed

---
+++ config/mongoose.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+
+//checking error if any
+main().catch(err => console.log(err));
+
+//connecting mangoose
+async function main() {
+  await mongoose.connect('mongodb+srv://pankajpurshotam:patel123@ecommerce.zwfgvl7.mongodb.net/?retryWrites=true&w=majority');
+
+}
+
+//connecting db to mangoose connection 
+const db = mongoose.connection;
+
+
+//checking successfully connected 
+db.once('open' , function(){
+    console.log('susscessfully connected to the database of rapidNotes');
+})
+
+module.exports = db;
